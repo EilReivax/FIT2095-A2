839f6166d3e9fbdb6e1dd5c3ea80c3c95dacc156
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-09-14 21:52:30 +1000

    Initial commit

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-const mongodb = require('mongodb');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const MongoClient = mongodb.MongoClient;
+const category = require('.routers/category');
+const event = require('.routers/event');
+
+const app = express();
+
+app.listen(8080);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+async function connect() {
+    await mongoose.connect('mongodb://localhost:27017/assignment-2');
+}
+connect().catch(err => console.log(err));
+
+app.post('/event', event.createOne);
+app.get('/event', event.getAll);
+app.delete('/event', event.deleteOne);
+app.put('/event', event.updateOne);
