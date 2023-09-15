538e109e2cd8e6c2e981f5cfaae23ac267105e13
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2023-09-16 07:15:14 +1000

    created async functions for a1 endpoints

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -3,22 +3,45 @@ const Category = require('../models/category');
 
 module.exports = {
     createOne: async function(req, res){
-        let categoryName = req.body.name;
-        let categoryDescription = req.body.description;
-        let categoryImage = req.body.image;
-        let newCategory = new Category(categoryName, categoryDescription, categoryImage);
+        let categoryDetails = req.body;
+        let newCategory = new Category(categoryDetails);
         await newCategory.save();
-        res.json(newCategory.id);
+        res.json(newCategory.categoryId);
     },
 
     getAll: async function(req, res){
         let categories = await Category.find()
-            .populate('eventList')
-            .exec();
+            // .populate('eventList')
+            // .exec();
+            res.json(categories);
     },
 
     deleteOne: async function(req, res){
-        let obj = await Category.findOneAndDelete({categoryId: req.body.id});
-        res.json();
+        let obj = await Category.deleteOne({categoryId: req.body.categoryId});
+        res.json(obj);
+    },
+
+    updateOne: async function(req, res){
+        let obj = await Category.findOneAndUpdate({categoryId: req.body.categoryId}, {name: req.body.name, description: req.body.description});
+        res.json(obj);
+    },
+
+    webCreateOne: async function(req, res){
+        let categoryDetails = req.body;
+        let newCategory = new Category(categoryDetails);
+        await newCategory.save();
+        res.redirect('/category/32528558/view-all');
+    },
+
+    webGetAll: async function(req, res){
+        let categories = await Category.find()
+            // .populate('eventList')
+            // .exec();
+            res.render("view-categories", {records: categories});
+    },
+
+    webDeleteOne: async function(req, res){
+        let obj = await Category.deleteOne({categoryId: req.body.categoryId});
+        res.redirect('category/32528558/view-all');
     }
 }
