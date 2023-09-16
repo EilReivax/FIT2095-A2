45e32c5fb493a81f6007aa9ef7380357bb5813d6
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-09-16 22:19:42 +1000

    Added populate category list

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -5,14 +5,24 @@ const Event = require('../models/event');
 
 module.exports = {
     createOne: async function (req, res) {
-        let newEvent = new Event(req.body);
+        let name = req.body.name;
+        let description = req.body.description;
+        let date = req.body.capacity;
+        let duration = req.body.duration;
+        let isActive = req.body.isActive;
+        let image = req.body.image;
+        let capacity = req.body.capacity;
+        let availability = req.body.availability;
+        let categoryList = req.body.categoryList.split(',');
+
+        let newEvent = new Event(name, description, date, duration, isActive, image, capacity, availability, categoryList);
         await newEvent.save();
         res.json(newEvent.eventId);
     },
     getAll: async function (req, res) {
         let events = await Event.find()
-        // .populate('categoryList')
-        // .exec();
+            .populate('categoryList')
+            .exec();
 
         res.json(events);
     },
@@ -37,21 +47,31 @@ module.exports = {
         });
     },
     webCreateOne: async function (req, res) {        
-        let newEvent = new Event(req.body);
+        let name = req.body.name;
+        let description = req.body.description;
+        let date = req.body.capacity;
+        let duration = req.body.duration;
+        let isActive = req.body.isActive;
+        let image = req.body.image;
+        let capacity = req.body.capacity;
+        let availability = req.body.availability;
+        let categoryList = req.body.categoryList.split(',');
+
+        let newEvent = new Event(name, description, date, duration, isActive, image, capacity, availability, categoryList);
         await newEvent.save();
         res.redirect("/event/michael/view-all");
     },
     webGetAll: async function (req, res) {        
         let events = await Event.find()
-        // .populate('categoryList')
-        // .exec();
+            .populate('categoryList')
+            .exec();
 
         res.render("view-events", {events: events});
     },
     webGetSoldout: async function (req, res) {        
         let events = await Event.find({availability: 0})
-        // .populate('categoryList')
-        // .exec();
+            .populate('categoryList')
+            .exec();
 
         res.render("view-events", {events: events});
     },
