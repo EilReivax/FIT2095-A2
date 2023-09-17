76371fd2046c0a43d00672527d7404d0859fd4aa
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-09-17 13:39:02 +1000

    Added stats

--- controllers/stats.js
+++ controllers/stats.js
@@ -5,17 +5,26 @@ const Operation = require("../models/operation");
 const OPERATION_ID = 'OPERATION_ID'
 
 module.exports = {
+    createOperation: async function (req, res) {
+        let operation = new Operation();
+        await operation.save();
+    },
     getAll: async function (req, res) {
-        let operation = await Operation.findById(OPERATION_ID);
+        operation = await Operation.findById(OPERATION_ID);
         let categoryCount = await Category.countDocuments();
         let eventCount = await Event.countDocuments();
         let createCount = operation.createCount;
         let updateCount = operation.updateCount;
         let deleteCount = operation.deleteCount;
+
+        console.log (`categoryCount: ${categoryCount} | eventCount: ${eventCount} | createCount: ${createCount} | updateCount: ${updateCount} | deleteCount: ${deleteCount}`);
+
         res.render("index", {
             categoryCount: categoryCount, 
             eventCount: eventCount,
-            
+            createCount: createCount,
+            updateCount: updateCount,
+            deleteCount: deleteCount
         });
     }
 }
